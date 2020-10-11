f139ad038280f01128afcb459c0b0cb5e173c8c0
netanel <pratn@post.bgu.ac.il>
2020-10-11 11:08:04 +0300

    Update income statement table view
    Update income statement table column view

--- src/main/java/Presentation/MainWindow.java
+++ src/main/java/Presentation/MainWindow.java
@@ -6,8 +6,10 @@ import System.SystemInterface.IncomeStatementDTO;
 import System.SystemInterface.SystemController;
 
 import javax.swing.*;
+import javax.swing.table.DefaultTableCellRenderer;
 import javax.swing.table.DefaultTableModel;
 import javax.swing.table.TableColumn;
+import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.util.List;
@@ -61,6 +63,7 @@ public class MainWindow extends JFrame {
         String[] columnsNames = {"Date", "Total revenue", "cost of revenue", "Gross profit",
                 "Operating expense", "Operating income", "Net income"};
         incomeStatementTable.setModel(new DefaultTableModel(columnsNames, 0));
+        incomeStatementTable.getColumnModel().getColumn(0).setCellRenderer(new ColumnColorRenderer(Color.getHSBColor(0,0, (float) 0.94), Color.BLACK));
     }
 
     private void setListeners(){
@@ -91,6 +94,22 @@ public class MainWindow extends JFrame {
                     tableModel.addRow(data);
                 }
             }
+        }
+    }
+
+    /*** table column models ***/
+    class ColumnColorRenderer extends DefaultTableCellRenderer {
+        Color backgroundColor, foregroundColor;
+        public ColumnColorRenderer(Color backgroundColor, Color foregroundColor) {
+            super();
+            this.backgroundColor = backgroundColor;
+            this.foregroundColor = foregroundColor;
+        }
+        public Component getTableCellRendererComponent(JTable table, Object value, boolean isSelected,   boolean hasFocus, int row, int column) {
+            Component cell = super.getTableCellRendererComponent(table, value, isSelected, hasFocus, row, column);
+            cell.setBackground(backgroundColor);
+            cell.setForeground(foregroundColor);
+            return cell;
         }
     }
 
