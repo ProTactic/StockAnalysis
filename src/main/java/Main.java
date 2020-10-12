5814f47b7d8e478dc079ac4c3aefb8f69002fd78
netanel <pratn@post.bgu.ac.il>
2020-10-12 08:54:32 +0300

    Edit boolean statement
    Edit boolean statemnet that check if the response is error or valid

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,7 +1,15 @@
+import Exceptions.StockSystemException;
 import Presentation.MainWindow;
+import System.SystemInterface.SystemController;
 
 public class Main {
-    public static void main(String[] args){
-        MainWindow mainWindow = new MainWindow();
+    public static void main(String[] args)  {
+        //MainWindow mainWindow = new MainWindow();
+        try {
+            SystemController systemController = SystemController.getInstance();
+        } catch (StockSystemException e) {
+            e.printStackTrace();
+        }
+
     }
 }
